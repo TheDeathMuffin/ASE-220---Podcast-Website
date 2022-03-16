0f2fd9d0c9e55ebf2d4243965874832a022d5a41
TheDeathMuffin <samlongTGLM1@gmail.com>
2022-03-15 21:29:09 -0400

    Moved contents to preload.js/postload.js

--- thisiswhoisloggedin.js
+++
@@ -1,3 +0,0 @@
-/* This is used to store the email for the currently-signed in user. It is a jenky but necessary (for now) form of authentication testing. */
-
-const currentUser = 'amy@gmail.com';          /* To simulate no user being signed in, set this value to null */
