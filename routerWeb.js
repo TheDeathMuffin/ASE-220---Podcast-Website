efa2d039fdbaa5c94072ae7a82c7450b89f74faf
TheDeathMuffin <samlongTGLM1@gmail.com>
2022-05-12 14:50:26 -0400

    Update routerWeb.js
    Added middleware to handle a route with no page specified ('/general/'). Set compact route director to check if file exists before delivering page.

--- routerWeb.js
+++ routerWeb.js
@@ -3,39 +3,20 @@ const router = express.Router()
 const fs=require('fs')
 
 //General Routes
+router.use('/',(req,res)=> {
+	console.log(req.params.page)
+	res.status(200).send(fs.readFileSync('./general/index.html','utf-8'))
+})
 
 //Compact route director.
-const generalRoutes = ["about","rules","terms","login","register","discipline","tags"];			//Place routes in here. Route name must correspond to the filename at the location ./general/
-router.use('/:pageName',(req, res)=> {
-	if (generalRoutes.includes(req.params.pageName)) {
-		res.status(200).send(fs.readFileSync('./general/' + req.params.pageName + '.html','utf-8'))
+const generalRoutes = ["about","rules","terms","login","register","discipline","tags"];		//Place routes in here. Route name must correspond to the filename at the location ./general/
+router.use('/:page',(req, res)=> {
+	if (generalRoutes.includes(req.params.page) && fs.existsSync('./general/' + req.params.page + '.html')) {	 //If route and corresponding HTML file exist, send appropriate page.
+		res.status(200).send(fs.readFileSync('./general/' + req.params.page + '.html','utf-8'))
 	}
 	else {
-		res.status(404).send(fs.readFileSync('./general/404.html','utf-8'))
+		res.status(404).send(fs.readFileSync('./general/404.html','utf-8'))		//If route or HTML file does not exist, send 404 page.
 	}
 })
 
-/*
-router.get('/about',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./general/about.html','utf-8'))
-})
-router.get('/rules',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./general/rules.html','utf-8'))
-})
-router.get('/terms',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./general/terms.html','utf-8'))
-})
-router.get('/login',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./general/login.html','utf-8'))
-})
-router.get('/register',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./general/register.html','utf-8'))
-})
-router.get('/discipline',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./disciplines/index.html','utf-8'))
-})
-router.get('/tags',(req, res)=>{
-	res.status(200).send(fs.readFileSync('./tags/index.html','utf-8'))
-})
-*/
 module.exports = router
