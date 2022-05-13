4bb29da5beabaf58ce056c825d7c72bee7c0019c
TheDeathMuffin <samlongTGLM1@gmail.com>
2022-05-13 17:10:30 -0400

    Update postload.js
    Uses our APIs to populate user data on the page.

--- assets/util/postload.js
+++ assets/util/postload.js
@@ -19,22 +19,17 @@ else {
 }
 
 /* Populates Page with User's Details */
-$.getJSON('https://jsonblob.com/api/jsonBlob/953096375785242624', function(data){
-    for (let i = 0; i < data.length; i++) {
-        if (currentUser == data[i]['email']) {
-            $('.currentName').append(data[i]['firstname'],' ',data[i]['lastname']);
-            $('.currentEmail').append(data[i]['email']);
-            $('.currentOrganization').append(data[i]['organization']);
-            if (!no_card_added) {
-                $('.card_no').text(data[i]['billing']['card_no']);
-                $('.name_on_card').text(data[i]['billing']['name_on_card']);
-                $('.billing_addr').text(data[i]['billing']['billing_addr']);
-                $('.floating-menu-billing-button').append(`<button type="button" class="btn btn-secondary">Edit</button>`);
-            }
-            else {
-                $('.floating-menu-billing-button').append(`<button type="button" class="btn btn-success">Add</button>`);
-            }
-            break;
-        }
+$.getJSON('http://localhost:8080/api/user/'+currentUserID, function(data){
+    $('.currentName').append(data['firstname'],' ',data['lastname']);
+    $('.currentEmail').append(data['email']);
+    $('.currentOrganization').append(data['organization']);
+    if (!no_card_added) {
+        $('.card_no').text(data[i]['billing']['card_no']);
+        $('.name_on_card').text(data['billing']['name_on_card']);
+        $('.billing_addr').text(data['billing']['billing_addr']);
+        $('.floating-menu-billing-button').append(`<button type="button" class="btn btn-secondary">Edit</button>`);
+    }
+    else {
+        $('.floating-menu-billing-button').append(`<button type="button" class="btn btn-success">Add</button>`);
     }
 });
