48181074a5f9b8fa8a1f07a9c65712b15759e1ba
TheDeathMuffin <samlongTGLM1@gmail.com>
2022-05-14 01:04:26 -0400

    Update routerAPI.js

--- routerAPI.js
+++ routerAPI.js
@@ -313,8 +313,6 @@ router.patch("/podcast/:id", getPodcast, async (req, res) => {
     }
     if(req.body.saves != null){
         res.podcast.saves = req.body.saves;
-        console.log('heyo');
-        console.log(res.podcast.saves);
     }
     try {
         //Saves all the updated information to the database then sends a response back to the requestor with a status of 200
